201b04040b03046f93e2ad27edcbbc6b8ddf74f3
Tarun kumar <sam332210@gmail.com>
2026-03-15 18:30:08 +0530

    working solution

--- .github/workflows/Scripts/run_tests.py
+++ .github/workflows/Scripts/run_tests.py
@@ -2,7 +2,7 @@ import os
 import json
 import subprocess
 
-working_dir = "D:\\MyProjects\\githubworkflow\\notepad"
+working_dir = "D:\\MyProjects\\githubworkflow\\aria2\\aria2-1.37.0-win-32bit-build1"
 file_path = "data.json"
 
 file_path = os.path.join(working_dir, file_path)
@@ -13,7 +13,7 @@ for key, value in data.items():
     print(f"data[{key}] = {value}")
 
 
-cmd = [os.path.join(working_dir, "notepad++.exe")] + ["data.json"]
+cmd = [os.path.join(working_dir, "aria2c.exe")] + ["https://github.com/notepad-plus-plus/notepad-plus-plus/releases/download/v8.9.2/npp.8.9.2.portable.x64.zip"]
 result = subprocess.run(cmd, cwd=working_dir, text=True)
 
 print("Return code:", result.returncode)
